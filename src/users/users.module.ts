3f730e3c84a539865b1b157be379ebe4238317c0
eunkyo3 <jyk8929@naver.com>
2026-02-22 23:48:37 +0900

    middleware

---
+++ src/users/users.module.ts
@@ -0,0 +1,9 @@
+import { Get, Module, Param } from '@nestjs/common';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+
+@Module({
+  controllers: [UsersController],
+  providers: [UsersService]
+})
+export class UsersModule {}
